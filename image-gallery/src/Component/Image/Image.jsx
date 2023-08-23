842ad53e4b88750f384eebe7db9f8f8d46a563d0
Sachin Sharma <sachinsharma.7thfeb@gmail.com>
2023-08-23 14:35:07 +0530

    useEffect()

--- image-gallery/src/Component/Image/Image.jsx
+++ image-gallery/src/Component/Image/Image.jsx
@@ -1,5 +1,11 @@
-function Image(){
+import './Image.css'
 
+function Image({title, image}){
+    return (
+        <div className='image'>
+            <img className='image-img' src={image} alt="image" />
+        </div>
+    )
 }
 
 export default Image;
--- image-gallery/src/Component/ImageList/ImageList.jsx
+++ image-gallery/src/Component/ImageList/ImageList.jsx
@@ -1,35 +1,34 @@
 import { useEffect, useState } from "react";
 import axios from 'axios';
 import './ImageList.css'
+import Image from "../Image/Image";
 
 function ImageList(){
 
     const [imageList, setImageList] = useState([]);
-    const [isLoading, setIsLoading] = useState(true);
 
     async function downloadImage(){
         
-        const response = await axios.get('https://api.slingacademy.com/v1/sample-data/photos')
-        const imageResults = response.data.photos;
-        
-        // const imageResultPromise = imageResults.map((photo)=> axios.get(photo.photos));
-        const imageData = await axios.all(imageResults); // array of 10 images detailed data
-        // console.log(imageData);
-        // console.log(response.data.photos);
+        const response = await axios.get('https://api.slingacademy.com/v1/sample-data/photos?limit=20'); // this downloads list of 10 images
 
-        const imageListResult = imageData.map((imageData)=>{
-            const image = imageData.photos;
+        const imageResults = response.data.photos;  // we get the array of images from the response
+        // console.log(imageResults);
+
+        // iterating over the array of imageResults that will return id, tittle, image, user of those 10 images
+        const imageListResult = imageResults.map((imageResults)=>{
+            // const image = imageResults.id;
+            // console.log(image);
             return {
-                id: image.id,
-                title: image.title, 
-                image: image.url, 
-                user: image.user
+                id: imageResults.id,
+                title: imageResults.title, 
+                image: imageResults.url, 
+                user: imageResults.user
             }
         })
 
-       console.log(imageListResult);
+        // console.log(imageListResult);
+        setImageList(imageListResult)
 
-        setIsLoading(false);
     }
 
     useEffect(()=>{
@@ -38,10 +37,11 @@ function ImageList(){
 
     return(
         <div className="image-list-wrapper">
-            <div>List of images</div>
-            {(isLoading) ? "Loading...." : "Data downloaded"}
+            {
+                imageList.map((i)=> <Image id={i.id} image={i.image} key={i.id}/>)
+            }
         </div>
     )
-}
+};
 
 export default ImageList;
